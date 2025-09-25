1ac91b105034910ac0d520630332c7fa4da5d528
Servidor <proyectosnuevos366@gmail.com>
2025-09-25 15:44:01 -0400

    Hemos creado los primeros componentes y tipamos

---
+++ src/components/Titulos.vue
@@ -0,0 +1,26 @@
+<template>
+  <component :is="`h${nivel}`" :class="estilo">
+    {{ texto }}
+  </component>
+</template>
+
+<script setup lang="ts">
+// Tipamos las props correctamente
+const props = defineProps<{
+  nivel: 1 | 2 | 3 | 4 | 5 | 6
+  texto: string
+}>()
+
+// Usamos una función para evitar el error de indexación
+const estilo = (() => {
+  const clases: Record<1 | 2 | 3 | 4 | 5 | 6, string> = {
+    1: 'text-4xl',
+    2: 'text-2xl font-semibold mb-4 text-center text-gray-800',
+    3: 'text-xl',
+    4: 'text-lg',
+    5: 'text-md',
+    6: 'text-sm'
+  }
+  return clases[props.nivel]
+})()
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
-  <h1 class="text-blue-500">Hola mundo, mi primer proyecto en vue!</h1>
+  <div className="container mx-auto min-h-dvh rounded-md  flex items-center justify-center gap-4 px-2">
+    <section className="flex flex-col items-center justify-center gap-4 min-h-[200px] sm:max-w-[400px] md:max-w-[600px] w-full bg-white border border-gray-300 rounded-md shadow-lg p-4">
+      <ImagenLogin indice="login" />
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
-  // No necesitas export default aquí.
-  // La lógica y el estado se declaran directamente.
-  import { defineOptions } from 'vue';
+import ImagenLogin from '../components/ImagenLogin.vue'
 
   defineOptions({
-    name: 'Home'
+    name: "Home",
   });
 </script>
 
